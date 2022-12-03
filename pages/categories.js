3f44a9164ca74909cc8af936d050c7b4082d3da0
minhluc073 <nguyenminhluc0703@gmail.com>
2022-12-03 16:16:46 +0700

    test br

--- pages/categories.js
+++ pages/categories.js
@@ -16,7 +16,7 @@ export async function getStaticProps() {
 export default function CategoriesPage({ categories }) {
   return (
     <React.Fragment>
-      <h1>Categories</h1>
+      <h2>Categories</h2>
 
       <CategoryList categories={categories} />
     </React.Fragment>
